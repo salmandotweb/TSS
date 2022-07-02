e8eccbf5cd14839570adf7de3df1b2d20e698c4a
Muhammad Salman <salmandotweb@gmail.com>
2022-07-02 14:39:54 +0500

    updated

---
+++ components/Home/CarouselCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "../../styles/Home/ReviewsSection.module.css";
+
+const CarouselCard = () => {
+	return (
+		<div className={classes.carouselCard}>
+			<h2>Latoya Gabbidon</h2>
+			<p>Owner - Candy Palazzo</p>
+			<p>
+				The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax
+				quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz,
+				vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab
+				quick-jived waltz. Brick quiz whangs jumpy veldt fox Fox nymphs grab
+				quick-jived waltz. Brick quiz whangs jumpy veldt fox
+			</p>
+		</div>
+	);
+};
+
+export default CarouselCard;
--- sections/HomePage/ReviewsSection.js
+++ sections/HomePage/ReviewsSection.js
@@ -1,7 +1,18 @@
 import React from "react";
+import CarouselCard from "../../components/Home/CarouselCard";
 import classes from "../../styles/Home/ReviewsSection.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ReviewsSection = () => {
+	var settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
 	return (
 		<section className={classes.reviewsSection}>
 			<div className={classes.info}>
@@ -15,7 +26,19 @@ const ReviewsSection = () => {
 					repellendus commodi ipsa voluptates!
 				</p>
 			</div>
-			<div className={classes.carousel}>Carousel</div>
+			<div className={classes.carousel}>
+				<Slider {...settings}>
+					<div>
+						<CarouselCard />
+					</div>
+					<div>
+						<CarouselCard />
+					</div>
+					<div>
+						<CarouselCard />
+					</div>
+				</Slider>
+			</div>
 		</section>
 	);
 };
