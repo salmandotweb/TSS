aa664f0a6581649bf96c6825d8c5bfb3f9844ddf
Muhammad Salman <salmandotweb@gmail.com>
2022-07-02 19:58:31 +0500

    updated

--- sections/HomePage/ReviewsSection.js
+++ sections/HomePage/ReviewsSection.js
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 const ReviewsSection = () => {
 	var settings = {
-		dots: true,
+		dots: false,
 		infinite: true,
 		speed: 500,
 		slidesToShow: 1,
