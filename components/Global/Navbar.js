78da07d233ec2a0eaa4179166aa54cf90a443e49
Muhammad Salman <salmandotweb@gmail.com>
2022-07-05 19:55:30 +0500

    updated

--- components/Global/Navbar.js
+++ components/Global/Navbar.js
@@ -1,14 +1,70 @@
 import React, { useState } from "react";
-import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
+import {
+	AiOutlineInstagram,
+	AiOutlineTwitter,
+	AiOutlineLine,
+} from "react-icons/ai";
 import { FiLinkedin } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import classes from "../../styles/Global/Navbar.module.css";
 
+const SubLinks = ({ link1, link2, link3, link4, link5, link6 }) => (
+	<div className={classes.subLinks}>
+		<p>{link1}</p>
+		<p>{link2}</p>
+		<p>{link3}</p>
+		<p>{link4}</p>
+		<p>{link5}</p>
+		<p>{link6}</p>
+	</div>
+);
+
 const Navbar = () => {
 	const [show, setShow] = useState(false);
+	const [home, setHome] = useState(false);
+	const [services, setServices] = useState(false);
+	const [blogs, setBlogs] = useState(false);
+	const [about, setAbout] = useState(false);
+	const [contact, setContact] = useState(false);
+
 	const handleShow = () => {
 		setShow(!show);
+	};
+	const handleHome = () => {
+		setHome(true);
+		setBlogs(false);
+		setAbout(false);
+		setContact(false);
+		setServices(false);
+	};
+	const handleServices = () => {
+		setHome(false);
+		setBlogs(false);
+		setAbout(false);
+		setContact(false);
+		setServices(true);
+	};
+	const handleBlogs = () => {
+		setHome(false);
+		setServices(false);
+		setAbout(false);
+		setContact(false);
+		setBlogs(true);
+	};
+	const handleAbout = () => {
+		setHome(false);
+		setServices(false);
+		setAbout(true);
+		setContact(false);
+		setBlogs(false);
+	};
+	const handleContact = () => {
+		setHome(false);
+		setServices(false);
+		setAbout(false);
+		setContact(true);
+		setBlogs(false);
 	};
 	return (
 		<nav className={classes.navbar}>
@@ -58,19 +114,45 @@ const Navbar = () => {
 						<div className={classes.right}>
 							<p>0203 876 6726</p>
 							<div className={classes.menu} onClick={handleShow}>
-								<HiOutlineMenuAlt2 />
+								<AiOutlineLine />
 							</div>
 						</div>
 					</nav>
 					<div className={classes.linksContainer}>
 						<div className={classes.links}>
-							<h1>Home</h1>
-							<h1>Services</h1>
-							<h1>Blogs</h1>
-							<h1>About Us</h1>
-							<h1>Contact Us</h1>
+							<h1
+								onClick={handleHome}
+								className={home ? classes.bold : classes.normal}>
+								Home
+							</h1>
+							<h1
+								onClick={handleServices}
+								className={services ? classes.bold : classes.normal}>
+								Services
+							</h1>
+							<h1
+								onClick={handleBlogs}
+								className={blogs ? classes.bold : classes.normal}>
+								Blogs
+							</h1>
+							<h1
+								onClick={handleAbout}
+								className={about ? classes.bold : classes.normal}>
+								About Us
+							</h1>
+							<h1
+								onClick={handleContact}
+								className={contact ? classes.bold : classes.normal}>
+								Contact Us
+							</h1>
 						</div>
-						<div className={classes.subLinks}></div>
+						<div className={classes.subLinksContainer}>
+							{(home && <SubLinks link1="Home" />) ||
+								(services && <SubLinks link1="Services" />) ||
+								(blogs && <SubLinks link1="Blogs" />) ||
+								(about && <SubLinks link1="About Us" />) ||
+								(contact && <SubLinks link1="Contact Us" />)}
+						</div>
 					</div>
 				</div>
 			)}
